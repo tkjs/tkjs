a0d25b2e1197aee6a8940dc2c264391a9d73fa1f
didadadida93 <didadadida93@gmail.com>
2020-02-02 21:05:04 +0700

    add variable so it more expressive

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const customMorgan = require('./middlewares/custom-morgan')
+const errorHandler = require('./middlewares/error-handler')
+const routes = require('./routes')
 const express = require('express')
 const app = express()
 
 app.use(customMorgan)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use('/', require('./routes'))
-app.use(require('./middlewares/error-handler'))
+app.use('/', routes)
+app.use(errorHandler)
 
 module.exports = app
