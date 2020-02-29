92d466917c14a097a63700dd905e8a992b18cf71
didadadida93 <didadadida93@gmail.com>
2020-02-29 07:17:09 +0700

    now app can login

--- server/api/login.js
+++ server/api/login.js
@@ -3,10 +3,6 @@ const qs = require('querystring')
 const ai = require('./axios-instance')
 const url = require('./urls')
 
-const regexGetMsid = /msid=([\w]*)&msname/g
-const regexGetToken = /token=([\w]*)&msid/g
-const regexGetSession = /%7B%22key%22%3A%22([\w]*)%22%2C%22id%22%3A%2226386%22%7D/g
-
 async function lobbyAuthentication(email, password) {
   if (!email) throw 'there is no email passed'
   if (!password) throw 'there is no password passed'
@@ -18,7 +14,7 @@ async function lobbyAuthentication(email, password) {
     cookies = ''
 
   response = await ai.get(url.getMsid)
-  msid = regexGetMsid.exec(response.data)[1]
+  msid = /msid=([\w]*)&msname/g.exec(response.data)[1]
 
   response = await ai.post(
     url.generateToken`msid: ${msid}`,
@@ -29,7 +25,7 @@ async function lobbyAuthentication(email, password) {
       },
     },
   )
-  token = regexGetToken.exec(response.data)[1]
+  token = /token=([\w]*)&msid/g.exec(response.data)[1]
 
   response = await ai.get(
     url.generateLobbySession`token: ${token}, msid: ${msid}`,
@@ -49,7 +45,7 @@ async function lobbyAuthentication(email, password) {
   })
 
   cookies += cookies + `; msid=${msid}`
-  lobbySession = regexGetSession.exec(lobbySession)[1]
+  lobbySession = /%7B%22key%22%3A%22([\w]*)%22%2C%22/g.exec(lobbySession)[1]
 
   // example on sending request using cookies and session
   // response = await ai.post(
@@ -72,8 +68,95 @@ async function lobbyAuthentication(email, password) {
   return [msid, lobbySession, cookies]
 }
 
-async function gameworldAuthentication(gameworld, msid, lobbySession, cookies) {
-  gameworld = gameworld.toLowerCase()
+async function gameworldAuthentication(worldName, msid, lobbySession, cookies) {
+  // get gameworld id based on worldName
+  let response,
+    gameworldId,
+    token,
+    gameworldSession,
+    gameworldCookies = ''
 
-  //
+  response = await ai.post(
+    url.lobbyApi,
+    {
+      action: 'get',
+      controller: 'cache',
+      params: {
+        names: ['Collection:Avatar'],
+      },
+      session: lobbySession,
+    },
+    {
+      headers: {
+        cookie: cookies,
+      },
+    },
+  )
+
+  gameworldId = response.data.cache[0].data.cache.find(
+    gameworld => gameworld.data.worldName === worldName.toUpperCase(),
+  ).data.consumersId
+
+  // login to gameworld
+  response = await ai.get(
+    url.joinGameworld`gameworldId: ${gameworldId}, msid: ${msid}`,
+  )
+  token = /token=([\w]*)&msid/g.exec(response.data)[1]
+
+  response = await ai.get(
+    url.generateGameworldToken`worldName: ${worldName}, token: ${token}, msid: ${msid}`,
+    {
+      maxRedirects: 0,
+      validateStatus: status => status >= 200 && status < 303,
+    },
+  )
+
+  response.headers['set-cookie'].forEach(cookieStr => {
+    if (cookieStr.includes('t5SessionKey')) {
+      gameworldSession = cookieStr.split(';')[0]
+    }
+
+    gameworldCookies += cookieStr.split(';')[0] + '; '
+  })
+
+  gameworldSession = /%7B%22key%22%3A%22([\w]*)%22%2C%22/g.exec(
+    gameworldSession,
+  )[1]
+
+  return [gameworldSession, gameworldCookies]
+}
+
+async function main(email, password, worldName) {
+  const [msid, lobbySession, lobbyCookies] = await lobbyAuthentication(
+    email,
+    password,
+  )
+
+  const [gameworldSession, gameworldCookies] = await gameworldAuthentication(
+    worldName,
+    msid,
+    lobbySession,
+    lobbyCookies,
+  )
+
+  const cookies = lobbyCookies + '; ' + gameworldCookies
+
+  const response = await ai.post(
+    `${url.gameworldApi`worldName: ${worldName}`}/?c=cache&a=get&t=${Date.now()}`,
+    {
+      action: 'get',
+      controller: 'cache',
+      params: {
+        names: ['Player'],
+      },
+      session: gameworldSession,
+    },
+    {
+      headers: {
+        cookie: cookies,
+      },
+    },
+  )
+
+  console.log(JSON.stringify(response.data, null, 2))
 }
--- server/api/urls.js
+++ server/api/urls.js
@@ -1,5 +1,7 @@
 const lobbyUrl = 'https://lobby.kingdoms.com/api'
 const mellonUrl = 'https://mellon-t5.traviangames.com'
+const gameworldUrl = (_, gameworld) =>
+  `https://${gameworld.toLowerCase()}.kingdoms.com`
 
 module.exports = {
   lobbyApi: `${lobbyUrl}/index.php`,
@@ -8,4 +10,10 @@ module.exports = {
     `${mellonUrl}/authentication/login/ajax/form-validate?msid=${msid}&msname=msid`,
   generateLobbySession: (_, token, msid) =>
     `${lobbyUrl}/login.php?token=${token}&msid=${msid}&msname=msid`,
+  gameworldApi: (_, gameworld) =>
+    `${gameworldUrl`gameworld: ${gameworld}`}/api`,
+  joinGameworld: (_, gameworldId, msid) =>
+    `${mellonUrl}/game-world/join/gameWorldId/${gameworldId}?msid=${msid}&msname=msid`,
+  generateGameworldToken: (_, worldName, token, msid) =>
+    `${gameworldUrl`gameworld: ${worldName}`}/api/login.php?token=${token}&msid=${msid}&msname=msid`,
 }
