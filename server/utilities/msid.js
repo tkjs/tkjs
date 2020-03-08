90f268c2427ea398a76b030b977e512ed58c2fe5
didadadida93 <didadadida93@gmail.com>
2020-03-08 13:58:56 +0700

    separating files based on functionality

---
+++ server/utilities/msid.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+
+const url = require('../constants/urls')
+const userAgent = require('../constants/user-agent')
+const { extractMsid } = require('./extractor')
+
+async function getMsid() {
+  const { data } = await axios.get(url.getMsid, { headers: { ...userAgent } })
+  return extractMsid(data)
+}
+
+module.exports = { getMsid }
