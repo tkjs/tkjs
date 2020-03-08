119a63df3607b1fc90b9f1ae91a496d5852f0b14
didadadida93 <didadadida93@gmail.com>
2020-03-08 12:54:04 +0700

    grouping files based on functionality

--- server/api/axios-instance.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-
-module.exports = axios.create({
-  headers: {
-    'User-Agent':
-      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0',
-  },
-})
---
+++ server/constants/user-agent.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'User-Agent':
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0',
+}
