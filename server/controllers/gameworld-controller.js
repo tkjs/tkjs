2ee6d48854451df67e3ab1ac425cb722550f7672
didadadida93 <didadadida93@gmail.com>
2020-03-08 13:57:16 +0700

    add controllers

---
+++ server/controllers/gameworld-controller.js
@@ -0,0 +1,40 @@
+const store = require('../store')
+const actions = require('../actions')
+
+class GameworldController {
+  static getState(request, response, next) {
+    const {
+      gameworld: { session, cookie, age },
+    } = store.getState()
+
+    if (session && cookie && age > new Date()) {
+      response.json({ status: 'Logged In', type: 'Gameworld' })
+    } else {
+      response.json({ status: 'Logged Out', type: 'Gameworld' })
+    }
+  }
+
+  static async authenticate(request, response, next) {
+    try {
+      const { gameworldId, worldName } = request.body
+      const gameworld = await gameworldAuthentication({
+        gameworldId,
+        worldName,
+      })
+
+      actions.gameworld.updateState(gameworld)
+      actions.account.updateWorldName(worldName)
+
+      response.json({ message: `UpdateState:LoginToGameworld 'success'` })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static resetState(request, response, next) {
+    actions.gameworld.resetState()
+    actions.account.resetWorldName()
+
+    response.json({ message: `UpdateState:LogoutFromGameworld 'success'` })
+  }
+}
---
+++ server/controllers/lobby-controller.js
@@ -0,0 +1,58 @@
+const store = require('../store')
+const actions = require('../actions')
+const {
+  getMsid,
+  getAvatarList,
+  encryptPassword,
+  lobbyAuthentication,
+} = require('../utilities')
+
+class LobbyController {
+  static getState(request, response, next) {
+    const {
+      lobby: { session, cookie, age },
+    } = store.getState()
+
+    if (session && cookie && age > new Date()) {
+      response.json({ status: 'Logged In', type: 'Lobby' })
+    } else {
+      response.json({ status: 'Logged Out', type: 'Lobby' })
+    }
+  }
+
+  static async getAvatar(request, response, next) {
+    try {
+      const avatarList = await getAvatarList()
+      response.json({ avatarList })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async authenticate(request, response, next) {
+    try {
+      let { email, body } = request.body
+      let msid = await getMsid()
+      let lobby = await lobbyAuthentication({ email, password, msid })
+
+      password = await encryptPassword(password)
+
+      actions.msid.updateMsid(msid)
+      actions.account.updateState({ email, password })
+      actions.lobby.updateState({ ...lobby })
+
+      response.json({ message: `UpdateState:LoginToLobby 'success'` })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static resetState(request, response, next) {
+    actions.msid.resetMsid()
+    actions.account.resetState()
+    actions.lobby.resetState()
+    response.json({ message: `UpdateState:LogoutFromLobby 'success'` })
+  }
+}
+
+module.exports = LobbyController
