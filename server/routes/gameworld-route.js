a8352ffb77cc86a85e36a1efb3840d01c0bbba30
didadadida93 <didadadida93@gmail.com>
2020-03-08 13:56:50 +0700

    group files based on functionality

--- server/routes/gameworld-route.js
+++ server/routes/gameworld-route.js
@@ -1,55 +1,8 @@
 const router = require('express').Router()
+const GameworldController = require('../controllers/gameworld-controller')
 
-const { gameworldAuthentication } = require('../api/login')
-const actions = require('../store/actions')
-const store = require('../store')
-
-router.get('/', function(req, res, next) {
-  const {
-    gameworld: { session, cookie, age },
-  } = store.getState()
-
-  res.json({
-    gameworldSession: session,
-    gameworldCookie: cookie,
-    gameworldAge: age,
-  })
-})
-
-router.post('/login', async function(req, res, next) {
-  try {
-    const {
-      msid,
-      lobby: { session, cookie },
-    } = store.getState()
-
-    if (!session || !cookie) {
-      throw { name: 'Forbidden', message: 'there is no session' }
-    }
-    const { gameworldId, worldName } = req.body
-
-    const gameworld = await gameworldAuthentication({
-      lobbySession: session,
-      lobbyCookie: cookie,
-      gameworldId,
-      worldName,
-      msid,
-    })
-
-    actions.gameworld.updateState(gameworld)
-    actions.account.updateWorldName(worldName)
-
-    res.json({ message: `UpdateState:LoginToGameworld 'success'` })
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.post('/logout', function(req, res, next) {
-  actions.gameworld.resetState()
-  actions.account.resetWorldName()
-
-  res.json({ message: `UpdateState:LogoutFromGameworld 'success'` })
-})
+router.get('/', GameworldController.getState)
+router.post('/authenticate', GameworldController.authenticate)
+router.post('/logout', GameworldController.resetState)
 
 module.exports = router
--- server/routes/lobby-route.js
+++ server/routes/lobby-route.js
@@ -1,69 +1,9 @@
 const router = require('express').Router()
+const LobbyController = require('../controllers/lobby-controller')
 
-const { lobbyAuthentication, getMsid, getAvatarList } = require('../api/login')
-const { encryptPassword } = require('../utilities/jwe-codec')
-const actions = require('../store/actions')
-const store = require('../store')
-
-router.get('/', function(req, res, next) {
-  const {
-    lobby: { session, cookie, age },
-  } = store.getState()
-
-  res.json({
-    lobbySession: session,
-    lobbyCookie: cookie,
-    lobbyAge: age,
-  })
-})
-
-router.get('/avatar-list', async function(req, res, next) {
-  try {
-    const {
-      lobby: { session, cookie },
-    } = store.getState()
-
-    if (!session || !cookie) {
-      throw { name: 'Forbidden', message: 'there is no session' }
-    }
-
-    let avatarList = await getAvatarList(session, cookie)
-    avatarList = avatarList.map(avatar => avatar.data)
-
-    res.json({ avatarList })
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.post('/login', async function(req, res, next) {
-  try {
-    let { email, password } = req.body
-    let msid = await getMsid()
-    let lobby = await lobbyAuthentication({
-      email,
-      password,
-      msid,
-    })
-
-    password = await encryptPassword(password)
-
-    actions.msid.updateMsid(msid)
-    actions.account.updateState({ email, password })
-    actions.lobby.updateState(lobby)
-
-    res.json({ message: `UpdateState:LoginToLobby 'success'` })
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.patch('/logout', function(req, res, next) {
-  actions.msid.resetMsid()
-  actions.account.resetState()
-  actions.lobby.resetState()
-
-  res.json({ message: `UpdateState:LogoutFromLobby 'success'` })
-})
+router.get('/', LobbyController.getState)
+router.get('/avatar-list', LobbyController.getAvatar)
+router.post('/authenticate', LobbyController.authenticate)
+router.patch('/logout', LobbyController.resetState)
 
 module.exports = router
