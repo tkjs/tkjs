14d2c7f5eafc2090d2f01e56e571d99ff7c83f94
didadadida93 <didadadida93@gmail.com>
2020-03-10 20:03:29 +0700

    add region ids of map to the constants

---
+++ server/constants/region-ids.js
@@ -0,0 +1,17 @@
+const { range, cellId } = require('../utilities')
+
+const regionIds = {}
+
+for (let x of range(-13, 14)) {
+  for (let y of range(-13, 14)) {
+    regionIds[cellId(x, y)] = []
+
+    for (let xx of range(0 + x * 7, 7 + x * 7)) {
+      for (let yy of range(0 + y * 7, 7 + y * 7)) {
+        regionIds[cellId(x, y)].push(cellId(xx, yy))
+      }
+    }
+  }
+}
+
+module.exports = regionIds
