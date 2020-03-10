22114fc08b72c574acd5c3c220fb0e41e7553e93
didadadida93 <didadadida93@gmail.com>
2020-03-10 20:03:53 +0700

    Add some utilites
    * cell-coordinates utilites is for reverse region id to x y coordinates
    * range is an utilites that mimic range() from python

---
+++ server/utilities/cell-coordinates.js
@@ -0,0 +1,21 @@
+module.exports = {
+  cellId: (x, y) => {
+    const rx = 536887296 + x
+    const ry = y * 32768
+    return rx + ry
+  },
+  reverseId: cellId => {
+    let xcord, ycord
+    let binary = parseInt(cellId, 10).toString(2)
+
+    if (binary.length < 30) binary = '0' + binary
+
+    xcord = binary.slice(15)
+    ycord = binary.slice(0, 15)
+
+    const realx = parseInt(xcord, 2) - 16384
+    const realy = parseInt(ycord, 2) - 16384
+
+    return [realx, realy]
+  },
+}
