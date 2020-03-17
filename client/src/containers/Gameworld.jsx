eaeccf57742c26bb0abc7320555a9a585815defc
Arief Rohman <didadadida93@gmail.com>
2020-03-17 07:21:10 +0700

    Update Front End
    * home page, lobby page, and gameworld page will check state of server first then update which page should be rendered

--- client/src/containers/Gameworld.jsx
+++ client/src/containers/Gameworld.jsx
@@ -1,9 +1,29 @@
 import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 import ai from "../api/axios-instance";
 import errorMessage from "../utilities/error-message";
 
 export default function Gameworld() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const main = async () => {
+      let data;
+
+      try {
+        ({ data } = await ai.get("/gameworlds"));
+        if (data.status !== "Logged In") {
+          history.push("/lobby");
+        }
+      } catch (err) {
+        errorMessage(err);
+      }
+    };
+
+    main();
+  });
+
   return (
     <div>
       <h1>Hello from gameworld</h1>
