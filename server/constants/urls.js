67b7e32e70233216b6b52a81b4e8afec27a2753b
Arief Rohman <didadadida93@gmail.com>
2020-03-18 18:19:20 +0700

    Update URL Constants
    I think it is better if I change the url into class so it is more
    readable.

--- server/constants/urls.js
+++ server/constants/urls.js
@@ -1,19 +1,66 @@
-const lobbyUrl = "https://lobby.kingdoms.com/api";
-const mellonUrl = "https://mellon-t5.traviangames.com";
-const gameworldUrl = (_, gameworld) =>
-  `https://${gameworld.toLowerCase()}.kingdoms.com`;
+const store = require("../store");
 
-module.exports = {
-  lobbyApi: `${lobbyUrl}/index.php`,
-  getMsid: `${mellonUrl}/authentication/login`,
-  generateToken: (_, msid) =>
-    `${mellonUrl}/authentication/login/ajax/form-validate?msid=${msid}&msname=msid`,
-  generateLobbySession: (_, token, msid) =>
-    `${lobbyUrl}/login.php?token=${token}&msid=${msid}&msname=msid`,
-  gameworldApi: (_, gameworld) =>
-    `${gameworldUrl`gameworld: ${gameworld}`}/api`,
-  generateGameworldToken: (_, gameworldId, msid) =>
-    `${mellonUrl}/game-world/join/gameWorldId/${gameworldId}?msid=${msid}&msname=msid`,
-  generateGameworldSession: (_, worldName, token, msid) =>
-    `${gameworldUrl`gameworld: ${worldName}`}/api/login.php?token=${token}&msid=${msid}&msname=msid`
-};
+class URL {
+  static get LOBBY_URL() {
+    return "https://lobby.kingdoms.com/api";
+  }
+
+  static get MELLON_URL() {
+    return "https://mellon-t5-traviangames.com";
+  }
+
+  static get GAMEWORLD_URL() {
+    const {
+      account: { worldName }
+    } = store.getState();
+
+    return "https://" + worldName.toLowerCase() + ".kingdoms.com";
+  }
+
+  static get LOBBY_API() {
+    return URL.LOBBY_URL + "/index.php";
+  }
+
+  static get GAMEWORLD_API() {
+    return URL.GAMEWORLD_URL + "/api";
+  }
+
+  static get GET_MSID() {
+    return URL.MELLON_URL + "/authentication/login";
+  }
+
+  static get GENERATE_LOBBY_TOKEN() {
+    const { msid } = store.getState();
+
+    return (
+      URL.MELLON_URL +
+      `/authentication/login/ajax/form-validate?msid=${msid}&msname=msid`
+    );
+  }
+
+  static GENERATE_LOBBY_SESSION(token) {
+    const { msid } = store.getState();
+
+    return URL.LOBBY_URL + `/login.php?token=${token}&msid=${msid}&msname=msid`;
+  }
+
+  static GENERATE_GAMEWORLD_TOKEN(gameworldId) {
+    const { msid } = store.getState();
+
+    return (
+      URL.MELLON_URL +
+      `/game-world/join/gameWorldId/${gameworldId}?msid=${msid}&msname=msid`
+    );
+  }
+
+  static GENERATE_GAMEWORLD_SESSION(token) {
+    const { msid } = store.getState();
+
+    return (
+      URL.GAMEWORLD_URL +
+      `/api/login.php?token=${token}&msid=${msid}&msname=msid`
+    );
+  }
+}
+
+module.exports = URL;
